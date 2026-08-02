c9cddc77810da392b6c3e0cb5d39c4ad813c889d
fayzan101 <faizanbasheer38@gmail.com>
2026-08-03 04:52:44 +0500

    refactor: update MainActivity and MainApplication for improved Expo integration and back button handling

--- android/app/src/main/java/com/remoteworktracker/MainActivity.kt
+++ android/app/src/main/java/com/remoteworktracker/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.remoteworktracker
 
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 
-import expo.modules.ReactActivityDelegateWrapper
-
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-class MainActivity : ReactActivity() {
+import expo.modules.ReactActivityDelegateWrapper
 
+class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    // Required for expo-splash-screen / themed launch.
+    setTheme(R.style.AppTheme)
+    super.onCreate(null)
 
     window.setFlags(
       WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
@@ -24,20 +25,37 @@ class MainActivity : ReactActivity() {
 
   /**
    * Returns the name of the main component registered from JavaScript.
+   * Expo AppEntry registers as "main".
    */
-  override fun getMainComponentName(): String = "RemoteWorkTracker"
+  override fun getMainComponentName(): String = "main"
 
   /**
-   * Returns the instance of the ReactActivityDelegate.
+   * Returns the instance of the [ReactActivityDelegate].
    */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-    ReactActivityDelegateWrapper(
+  override fun createReactActivityDelegate(): ReactActivityDelegate {
+    return ReactActivityDelegateWrapper(
       this,
       BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-      DefaultReactActivityDelegate(
+      object : DefaultReactActivityDelegate(
         this,
         mainComponentName,
         fabricEnabled
-      )
+      ) {}
     )
+  }
+
+  /**
+   * Align the back button behavior with Android S
+   * where moving root activities to background instead of finishing activities.
+   */
+  override fun invokeDefaultOnBackPressed() {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+      if (!moveTaskToBack(false)) {
+        super.invokeDefaultOnBackPressed()
+      }
+      return
+    }
+
+    super.invokeDefaultOnBackPressed()
+  }
 }
